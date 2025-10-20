d50ec56f0c1e84b6f18f24009acee67c32a6baad
Yamlak Negash <yamlaknegash96@gmail.com>
2025-10-21 00:48:53 +0300

    Message controller finished.

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
+
+router.get("/:id", protectRoute, getMessages); //user's id we'd like to fetch our messages.
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
