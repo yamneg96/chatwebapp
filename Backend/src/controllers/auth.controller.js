9c95f08e7ac470073d36e22ac505740ed12f45ee
Yamlak Negash <yamlaknegash96@gmail.com>
2025-10-20 22:48:23 +0300

    Login and Logout logic done successfully.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
+
   const {fullName, email, password} = req.body;
 
   try {
@@ -50,10 +51,41 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async(req, res) => {
+
+  const {email, password} = req.body;
+
+  try {
+    const user = await User.findOne({email});
+
+    if(!user) {
+      return res.status(400).json({message: "Email address not registerd."});
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if(!isPassword) {
+      return res.status(400).json({message: "Password Incorrect."});
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id : user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"});
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});    
+  }
 };
